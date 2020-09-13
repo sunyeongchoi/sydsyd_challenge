e8fc6bc1bffa56be3666d1c206f2fbf705011931
sunyeongchoi <sn0716@naver.com>
2020-09-13 15:41:32 +0900

    programmers triangle 문제

---
+++ argorithm/triangle.py
@@ -0,0 +1,16 @@
+def solution(triangle):
+    answer = 0
+    for i in range(1, len(triangle)):
+        for j in range(i+1):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == i:
+                triangle[i][j] += triangle[i-1][-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1],triangle[i-1][j])
+    return max(triangle[-1])
+
+
+if __name__ == '__main__':
+    real_answer = solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]])
+    print(real_answer)
