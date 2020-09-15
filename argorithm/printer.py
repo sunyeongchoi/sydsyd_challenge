fb0ad1b88962127c33476d2e616f69b367f7edc4
sunyeongchoi <sn0716@naver.com>
2020-09-15 23:39:30 +0900

    programmers printer문제 파일만 생성...

---
+++ argorithm/printer.py
@@ -0,0 +1,9 @@
+def solution(priorities, location):
+    answer = 0
+    if priorities.pop(0)
+    return answer
+
+
+if __name__ == '__main__':
+    real_answer = solution()
+    print(real_answer)
