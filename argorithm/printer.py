afdc50d2f5da7c4edad74514f02dc1078627d8e9
sunyeongchoi <sn0716@naver.com>
2020-09-16 23:18:28 +0900

    programmers printer문제 푸는중

--- argorithm/printer.py
+++ argorithm/printer.py
@@ -1,9 +1,12 @@
 def solution(priorities, location):
     answer = 0
-    if priorities.pop(0)
+    value = priorities[location]
+    for i in range(len(priorities)):
+        if value <= priorities.pop(0):
+            answer += 1
     return answer
 
 
 if __name__ == '__main__':
-    real_answer = solution()
+    real_answer = solution([1, 1, 9, 1, 1, 1],0)
     print(real_answer)
