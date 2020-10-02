f608d04855d273b0bc60d4c8c1267293be1a171c
sunyeongchoi <sn0716@naver.com>
2020-10-02 22:12:20 +0900

    프로그래머스 가장큰수

---
+++ argorithm/biggest_number.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = sorted(list(map(str, numbers)), key=lambda x: x*5, reverse=True)
+    return str(int(''.join(answer)))
+
+
+if __name__ == '__main__':
+    real_answer = solution([6, 10, 2])
+    print(real_answer)
