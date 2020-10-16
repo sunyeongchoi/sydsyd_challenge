1e670601564b3ab3ac63b2c41b883a76d2830b09
sunyeongchoi <sn0716@naver.com>
2020-10-16 23:33:54 +0900

    h-index풀어보자

---
+++ argorithm/h-index.py
@@ -0,0 +1,11 @@
+def solution(citations):
+    answer = 0
+    while True:
+        for i in sorted(citations):
+            pass
+    return answer
+
+
+if __name__ == '__main__':
+    real_answer = solution([3, 0, 6, 1, 5])
+    print(real_answer)
