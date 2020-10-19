7af5a205e9419c130c49d7e0a7b345cd2fb417e4
sunyeongchoi <sn0716@naver.com>
2020-10-19 22:00:09 +0900

    트럭 알고리즘

---
+++ argorithm/truck.py
@@ -0,0 +1,8 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    return answer
+
+
+if __name__ == 'main':
+    real_answer = solution(2, 10, [7,4,5,6])
+    print(real_answer)
