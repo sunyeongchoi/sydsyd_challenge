28dd9b89b48fa8a2baea23775580d2e5b86eadb8
sunyeongchoi <sn0716@naver.com>
2020-10-21 01:04:56 +0900

    트럭문제 푸는중

--- argorithm/truck.py
+++ argorithm/truck.py
@@ -1,12 +1,27 @@
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     present = [0] * bridge_length
+    current_weight = 0
+
     while truck_weights:
-        present.append(truck_weights.pop(0))
+        answer += 1
+        bridge_out_weight = present.pop(0)
+        current_weight -= bridge_out_weight
 
+        if weight < current_weight + present[0]:
+            present.append(0)
 
+        else:
+            next_truck = truck_weights.pop(0)
+            present.append(next_truck)
+            current_weight += next_truck
 
-    return val
+    while current_weight > 0:
+        answer += 1
+        bridge_out_weight = present.pop(0)
+        current_weight -= bridge_out_weight
+
+    return answer
 
 
 if __name__ == '__main__':
