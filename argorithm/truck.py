8610fa0dbe11f55f16650088176fa1eeba640797
sunyeongchoi <sn0716@naver.com>
2020-10-20 23:04:41 +0900

    truck문제 푸는중

--- argorithm/truck.py
+++ argorithm/truck.py
@@ -1,8 +1,14 @@
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    return answer
+    present = [0] * bridge_length
+    while truck_weights:
+        present.append(truck_weights.pop(0))
 
 
-if __name__ == 'main':
-    real_answer = solution(2, 10, [7,4,5,6])
+
+    return val
+
+
+if __name__ == '__main__':
+    real_answer = solution(2, 10, [7, 4, 5, 6])
     print(real_answer)
