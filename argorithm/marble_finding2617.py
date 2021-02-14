21dc1abe26ac22f04a9aea66987233a9c388aab5
sunyeongchoi <sn0716@naver.com>
2021-02-15 00:50:19 +0900

    구슬찾기 알고리즘

--- argorithm/marble_finding2617.py
+++ argorithm/marble_finding2617.py
@@ -1,22 +1,32 @@
-def dfs(node, depth):
+from collections import defaultdict
+
+def dfs(node, weight):
+    global cnt
     visited[node] = True
-    for n in graph[node]:
-        dfs(n, depth+1)
-    cnt[node] = depth
+    for n in weight[node]:
+        if not visited[n]:
+            cnt += 1
+            dfs(n, weight)
+    return cnt
 
 if __name__ == '__main__':    
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(M)]
-    graph = [[] for _ in range(N+1)]
-    visited = [False]*(N+1)
-    cnt = [0]*(N+1)
+    result = 0
+
+    light = defaultdict(list)
+    heavy = defaultdict(list)
 
     for x, y in arr:
-        graph[x].append(y)
+        heavy[x].append(y)
+        light[y].append(x)
 
     for i in range(1, N+1):
-        if not visited[i]:
-            dfs(i, 0)
-    print(cnt)
-    print(max(cnt))
-
+        visited = [False]*(N+1)
+        cnt = 0
+        if dfs(i, heavy) >= (N+1)//2:
+            result += 1
+        cnt = 0
+        if dfs(i, light) >= (N+1)//2:
+            result += 1
+    print(result)
