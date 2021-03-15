42c8bd193a9454c8a9c34426a57c08fe62aba3de
sunyeongchoi <sn0716@naver.com>
2021-03-15 23:06:47 +0900

    알고리즘

---
+++ argorithm/13459.py
@@ -0,0 +1,11 @@
+from collections import deque
+import sys; input=sys.stdin.readline
+n, m = map(int, input().split())
+arr = [list(input().strip()) for _ in range(n)]
+
+def bfs():
+    queue = deque()
+    while queue:
+        pass
+
+
